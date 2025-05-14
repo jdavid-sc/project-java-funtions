be5d0905ce851c7e2280a95cb30e9a3157649184
David Saldarriaga <david.salda98@gmail.com>
2025-05-13 19:06:43 -0500

    función sin parámetros y sin retorno implementada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int total = sumar(3,4);
+        System.out.println(total);
     }
+
+    public static int sumar(int a, int b) {
+    return a + b;
+}
 }
