14f63b6051be8aa9a9d20dedd3da39f965f9815d
David Saldarriaga <david.salda98@gmail.com>
2025-05-13 20:16:47 -0500

    función con parámetros y con retorno implementada

---
+++ src/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int total = RetornarMultiplicacion(3,8);
+        System.out.println("El total de la multiplicaion: " + total);
+    }
+
+    public static int RetornarMultiplicacion(int a, int b){
+        return a * b;
+    }
+}
