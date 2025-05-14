d97becc62a32000bb99a7d493dc79f9411222338
David Saldarriaga <david.salda98@gmail.com>
2025-05-13 20:04:10 -0500

    función con parámetros y sin retorno implementada

---
+++ src/PrintSum.java
@@ -0,0 +1,10 @@
+public class PrintSum {
+    public static void main(String[] args) {
+        sumar(3,3);
+    }
+
+    public static void  sumar(int a, int b){
+        int suma = a + b;
+        System.out.println("El total de la suma es: " + suma);
+    }
+}
