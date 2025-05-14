9bcdcfc3e15f1a394d136367a1fdd6a73495dda8
David Saldarriaga <david.salda98@gmail.com>
2025-05-13 20:09:33 -0500

    función sin parámetros y con retorno implementada

---
+++ src/RandomNumber.java
@@ -0,0 +1,10 @@
+public class RandomNumber {
+    public static void main(String[] args) {
+        int numero = retornarNumero();
+        System.out.println(numero);
+    }
+
+    public static int retornarNumero(){
+        return 23;
+    }
+}
